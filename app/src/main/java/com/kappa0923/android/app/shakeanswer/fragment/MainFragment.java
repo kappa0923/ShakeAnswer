7b89add1c7436b5ea6a7b38796c4650fd2ab2ab0
kappa0923 <tom.kappa0923@gmail.com>
2016-09-06 01:08:08 +0900

    SwitchでServiceを起動

--- app/src/main/java/com/kappa0923/android/app/shakeanswer/fragment/MainFragment.java
+++ app/src/main/java/com/kappa0923/android/app/shakeanswer/fragment/MainFragment.java
@@ -1,13 +1,23 @@
 package com.kappa0923.android.app.shakeanswer.fragment;
 
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.kappa0923.android.app.shakeanswer.R;
+import com.kappa0923.android.app.shakeanswer.services.BackgroundService;
+
+import java.util.List;
 
 /**
  * 起動時画面の表示要Fragment
@@ -27,4 +37,36 @@ public class MainFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Switch serviceSwitch = (Switch)view.findViewById(R.id.service_switch);
+        serviceSwitch.setChecked(isServiceRunning());
+        serviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    getActivity().startService(new Intent(getContext(), BackgroundService.class));
+                } else {
+                    getActivity().stopService(new Intent(getContext(), BackgroundService.class));
+                }
+            }
+        });
+    }
+
+    /**
+     * サービスが起動中かどうかを検出
+     * @return {@code true}起動中
+     */
+    private boolean isServiceRunning() {
+        ActivityManager manager = (ActivityManager)getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        List<RunningServiceInfo> serviceInfoList = manager.getRunningServices(Integer.MAX_VALUE);
+        for (RunningServiceInfo info : serviceInfoList) {
+            if (TextUtils.equals(info.service.getClassName(), BackgroundService.class.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
