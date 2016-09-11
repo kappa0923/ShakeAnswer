3afdf5d5760e739ccf700d57c0a532a5d86e8944
tomoyasu <tom.kappa0923@gmail.com>
2016-09-12 01:01:27 +0900

    電話への応答

--- app/src/main/java/com/kappa0923/android/app/shakeanswer/services/AnswerPhoneService.java
+++ app/src/main/java/com/kappa0923/android/app/shakeanswer/services/AnswerPhoneService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.view.KeyEvent;
 
 import com.kappa0923.android.app.shakeanswer.R;
 import com.kappa0923.android.app.shakeanswer.common.CallReceiver;
@@ -34,7 +35,14 @@ public class AnswerPhoneService extends Service implements CallReceiver.CallStat
 
     @Override
     public void onRinging() {
-
+        Intent buttonDown = new Intent(Intent.ACTION_MEDIA_BUTTON)
+                .putExtra(Intent.EXTRA_KEY_EVENT,
+                        new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+        Intent buttonUp = new Intent(Intent.ACTION_MEDIA_BUTTON)
+                .putExtra(Intent.EXTRA_KEY_EVENT,
+                        new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
+        getApplicationContext().sendOrderedBroadcast(buttonDown, null);
+        getApplicationContext().sendOrderedBroadcast(buttonUp, null);
     }
 
     @Override
